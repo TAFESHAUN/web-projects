27f4c218a710ee731693c69bb937bb9896116118
Shaun Gregory Stummer <141109489+TAFESHAUN@users.noreply.github.com>
2026-03-10 10:06:25 +1100

    Update with date and time

--- wk-6/test.js
+++ wk-6/test.js
@@ -1,6 +1,10 @@
 //I can write JavaScript
 console.log("START OF TEST");
 
+//let sometxt = "template literal text test";
+
+//alert(`Whatever I want + some liteal temaplate text:${sometxt}`);
+
 let petName = "Art";
 var petAge = 3;
 const shelterName = "Pawsome Pets";
@@ -20,5 +24,24 @@ function testGreet() {
 
     document.getElementById("tgreet").textContent = msg;
 }
+
+//Function for testing USER updates to the page
+function ShowDOMmsg(){
+    //console.log('Log if NEW BUTTON worked'); //Quick test to see if script is linked
+    const today = new Date();
+    const fmtDate = today.toLocaleDateString(); 
+    const fmtTime = today.toLocaleTimeString();
+
+    //TIME
+    let msgTime = `Page is updated at: ${fmtTime}`;
+
+    //DATE
+    let msgDate = `Page is updated today at: ${fmtDate}`
+
+    let outputMsg = `Time: ${msgTime} \n Date:${msgDate}` //fix new line
+
+    document.getElementById('testOutput').textContent = outputMsg;
+}
+
 
 console.log("END OF TEST");
